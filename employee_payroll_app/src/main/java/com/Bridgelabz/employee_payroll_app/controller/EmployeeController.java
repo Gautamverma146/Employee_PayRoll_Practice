cfe87486e730e71d7d5d5eaf329bc65cf3439110
Gautamverma2003 <gautam146official@gmail.com>
2025-02-28 00:37:57 +0530

    [Gautam] add.Added Rest controller with different Http method

---
+++ employee_payroll_app/src/main/java/com/Bridgelabz/employee_payroll_app/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz.employee_payroll_app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+     @PostMapping
+     @GetMapping
+     @PutMapping
+     @DeleteMapping
+}
