c338211f7839a5a1ed5ae048dfae94b98454de20
Gautamverma2003 <gautam146official@gmail.com>
2025-02-27 23:52:45 +0530

    [Gautam] add.Added Set-up Employee Payroll application

---
+++ employee_payroll_app/src/test/java/com/Bridgelabz/employee_payroll_app/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.employee_payroll_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
