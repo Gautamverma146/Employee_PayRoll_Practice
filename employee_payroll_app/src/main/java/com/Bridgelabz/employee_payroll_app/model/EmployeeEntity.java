2d666510b32caa6d157fcad681d19b816b24a868
Gautamverma2003 <gautam146official@gmail.com>
2025-02-28 01:12:45 +0530

    [Gautam] add.Added Dto and Model(Entity)

---
+++ employee_payroll_app/src/main/java/com/Bridgelabz/employee_payroll_app/model/EmployeeEntity.java
@@ -0,0 +1,22 @@
+package com.Bridgelabz.employee_payroll_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double salary;
+
+}
